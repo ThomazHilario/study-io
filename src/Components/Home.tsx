7d1a51f414e1beaaa68eab44b5528785adc2132d
Thomaz Alves <thomazhilario5@gmail.com>
2024-03-04 17:39:22 -0300

    Criando animação de loading

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 // imports react-router-dom
 import { Link } from 'react-router-dom'
 
@@ -40,9 +42,15 @@ export const Home = () => {
     // Context
     const { setId, setDataUser } = UseMyContext()
 
+    // state - loading
+    const [loading, setLoading] = useState(false)
+
     // sing in
     async function singIn(data:LoginType){
         try {
+            // Alterando o valor do loading
+            setLoading(true)
+
             // Autenticando usuario
             const user = await signInWithEmailAndPassword(auth, data.email, data.password)
 
@@ -73,6 +81,8 @@ export const Home = () => {
                 }
             })
 
+        }finally{
+            setLoading(false)
         }
     }
 
@@ -80,60 +90,68 @@ export const Home = () => {
         <RightClick>
             <main className=" flex justify-center items-center h-screen bg-[#5356ad]">
                 {/* container do formulario de login */}
-                <div className=" text-white flex justify-center items-center h-[90vh] w-[90vw] rounded-2xl bg-[#202124]">
-
-                    <div className="w-1/2 flex flex-col justify-center items-center gap-10 ">
-                        <h1 className="leading-[3.5rem] font-system text-5xl w-80 text-center">
-                            Bem-vindo ao <strong className='bg-clip-text text-transparent bg-gradient-to-r from-violet-500 to-fuchsia-500'>Study-io</strong>
-                        </h1>
-                        <p className="font-system text-[1.1rem] w-[36vw] text-justify">👉 O Study-io é um aplicativo de produtividade cuidadosamente projetado para estudantes e trabalhadores de todas as áreas e níveis de ensino.</p>
-
-                        <p className="font-system text-[1.1rem] w-[36vw] text-justify">👉 Com o Study-io, você pode criar, organizar e priorizar suas tarefas de estudo e trabalho em uma interface simples e intuitiva. Nunca mais perca o foco do que realmente importa.</p>
-
-                        <h1  className="font-roboto text-[2.5rem] font-medium text-wrap text-center">🌌Venha Conosco!🌌</h1>
+                {loading ? (
+                    <div className=" text-white flex justify-center items-center h-[90vh] w-[90vw] rounded-2xl bg-[#202124]">
+                        <button type="button" className="bg-indigo-600 flex justify-center items-center px-7 py-3" disabled>
+                            <svg className="border-[9px] rounded-full border-dotted border-white animate-spin size-9 mr-3 " viewBox="0 0 24 24"/>
+                            <h1 className='text-2xl font-system'>Carregando...</h1>
+                        </button>
                     </div>
-                    
-                    <form className="w-1/2 flex flex-col justify-center items-center" 
-                    onSubmit={handleSubmit(singIn)}>
-                        <legend className='mb-7 font-bold font-roboto text-3xl'>Sing In</legend>
+                ) : (
+                    <div className=" text-white flex justify-center items-center h-[90vh] w-[90vw] rounded-2xl bg-[#202124]">
+                        <div className="w-1/2 flex flex-col justify-center items-center gap-10 ">
+                            <h1 className="leading-[3.5rem] font-system text-5xl w-80 text-center">
+                                Bem-vindo ao <strong className='bg-clip-text text-transparent bg-gradient-to-r from-violet-500 to-fuchsia-500'>Study-io</strong>
+                            </h1>
+                            <p className="font-system text-[1.1rem] w-[36vw] text-justify">👉 O Study-io é um aplicativo de produtividade cuidadosamente projetado para estudantes e trabalhadores de todas as áreas e níveis de ensino.</p>
 
-                        <div className="flex flex-col gap-2 mb-5">
-                            <label className="text-lg">Email:</label>
-                            <input type="text" 
-                            placeholder="Digite seu email..." 
-                            className={`text-black rounded-sm w-[25vw] outline-none pl-2 py-2 ${errors.email ? 'border-2 border-red-500' : 'border-none'}`}
-                            {...register('email')}/>
-                            
-                            {/* validação de erros dos inputs */}
-                            {errors.email && 
-                                <p className='text-red-500'>
-                                    {errors.email.message}
-                                </p>}
+                            <p className="font-system text-[1.1rem] w-[36vw] text-justify">👉 Com o Study-io, você pode criar, organizar e priorizar suas tarefas de estudo e trabalho em uma interface simples e intuitiva. Nunca mais perca o foco do que realmente importa.</p>
+
+                            <h1  className="font-roboto text-[2.5rem] font-medium text-wrap text-center">🌌Venha Conosco!🌌</h1>
                         </div>
+                        
+                        <form className="w-1/2 flex flex-col justify-center items-center" 
+                        onSubmit={handleSubmit(singIn)}>
+                            <legend className='mb-7 font-bold font-roboto text-3xl'>Sing In</legend>
 
-                        <div className="flex flex-col gap-2 mb-5">
-                            <label className="text-lg">Password:</label>
-                            <input type="password" 
-                            placeholder="Digite sua senha" 
-                            className={` text-black rounded-sm w-[25vw] outline-none pl-2 py-2 ${errors.password ? 'border-2 border-red-500' : 'border-none'}`}
-                            {...register('password')}/>
-                            
-                            {/* validação de erros dos inputs */}
-                            {errors.password && 
-                                <p className='text-red-500'>
-                                    {errors.password.message}
-                                </p>}
-                        </div>
+                            <div className="flex flex-col gap-2 mb-5">
+                                <label className="text-lg">Email:</label>
+                                <input type="text" 
+                                placeholder="Digite seu email..." 
+                                className={`text-black rounded-sm w-[25vw] outline-none pl-2 py-2 ${errors.email ? 'border-2 border-red-500' : 'border-none'}`}
+                                {...register('email')}/>
+                                
+                                {/* validação de erros dos inputs */}
+                                {errors.email && 
+                                    <p className='text-red-500'>
+                                        {errors.email.message}
+                                    </p>}
+                            </div>
 
-                        <div className="flex flex-col gap-2 mt-2">
-                            <button className="text-white bg-[#5356ad] w-[25vw] py-2 rounded-sm">
-                                Entrar
-                            </button>
+                            <div className="flex flex-col gap-2 mb-5">
+                                <label className="text-lg">Password:</label>
+                                <input type="password" 
+                                placeholder="Digite sua senha" 
+                                className={` text-black rounded-sm w-[25vw] outline-none pl-2 py-2 ${errors.password ? 'border-2 border-red-500' : 'border-none'}`}
+                                {...register('password')}/>
+                                
+                                {/* validação de erros dos inputs */}
+                                {errors.password && 
+                                    <p className='text-red-500'>
+                                        {errors.password.message}
+                                    </p>}
+                            </div>
 
-                            <p className='text-center'>Não possui uma conta ? <Link to='/register' className='text-violet-600'>Crie agora</Link></p>
-                        </div>
-                    </form>
-                </div>
+                            <div className="flex flex-col gap-2 mt-2">
+                                <button className="text-white bg-[#5356ad] w-[25vw] py-2 rounded-sm">
+                                    Entrar
+                                </button>
+
+                                <p className='text-center'>Não possui uma conta ? <Link to='/register' className='text-violet-600'>Crie agora</Link></p>
+                            </div>
+                        </form>
+                    </div>
+                )}
             </main>
         </RightClick>
     )
--- src/Components/Register.tsx
+++ src/Components/Register.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 // import recat-router-dom
 import { Link } from 'react-router-dom'
 
@@ -46,9 +48,15 @@ export const Register = () => {
     // Context
     const { setId, setDataUser } = UseMyContext()
 
+    // state - loading
+    const [loading, setLoading] = useState(false)
+
     // sing Up
     async function singUp(data:RegisterType){
         try {
+            // Alterando o valor do loading
+            setLoading(true)
+
             // Criando autenticacao do usuario
             const user = await createUserWithEmailAndPassword(auth, data.email, data.password)
 
@@ -75,6 +83,8 @@ export const Register = () => {
                     )
                 }
             }
+        }finally{
+            setLoading(false)
         }
     }
 
@@ -82,78 +92,87 @@ export const Register = () => {
         <RightClick>
             <main className=" flex justify-center items-center h-screen bg-[#5356ad]">
                 {/* container do formulario de login */}
-                <div className=" text-white flex justify-center items-center h-[90vh] w-[90vw] rounded-2xl bg-[#202124]">
-
-                    <div className="w-1/2 flex flex-col justify-center items-center gap-10 ">
-                        <h1 className=" leading-[3.5rem] font-system text-5xl text-wrap w-80 text-center">
-                            Bem-vindo ao <strong className='bg-clip-text text-transparent bg-gradient-to-r from-violet-500 to-fuchsia-500'>Study-io</strong>
-                        </h1>
-                        <p className="font-system text-[1.1rem] w-[36vw] text-justify">👉 O Study-io é um aplicativo de produtividade cuidadosamente projetado para estudantes e trabalhadores de todas as áreas e níveis de ensino.</p>
-
-                        <p className="font-system text-[1.1rem] w-[36vw] text-justify">👉 Com o Study-io, você pode criar, organizar e priorizar suas tarefas de estudo e trabalho em uma interface simples e intuitiva. Nunca mais perca o foco do que realmente importa.</p>
-
-                        <h1  className="font-roboto text-[2.5rem] font-medium text-wrap text-center">🌌Venha Conosco!🌌</h1>
+                {loading ? (
+                    <div className=" text-white flex justify-center items-center h-[90vh] w-[90vw] rounded-2xl bg-[#202124]">
+                        <button type="button" className="bg-indigo-600 flex justify-center items-center px-7 py-3" disabled>
+                            <svg className="border-[9px] rounded-full border-dotted border-white animate-spin size-9 mr-3 " viewBox="0 0 24 24"/>
+                            <h1 className='text-2xl font-system'>Carregando...</h1>
+                        </button>
                     </div>
+                ) : (
+                    <div className=" text-white flex justify-center items-center h-[90vh] w-[90vw] rounded-2xl bg-[#202124]">
 
-                    <form className="w-1/2 flex flex-col justify-center items-center" onSubmit={handleSubmit(singUp)}>
-                        <legend className='mb-7 font-bold font-roboto text-3xl'>Sing Up</legend>
-                        
-                        <div className="flex flex-col gap-2 mb-5">
-                            <label className="text-lg">Username:</label>
-                            <input type="text" 
-                            placeholder="Digite seu nome..." 
-                            className={`text-black rounded-sm w-[25vw] outline-none pl-2 py-2 ${errors.username ? 'border-2 border-red-500' : 'border-none'}`} 
-                            {...register("username")}
-                            />
+                        <div className="w-1/2 flex flex-col justify-center items-center gap-10 ">
+                            <h1 className=" leading-[3.5rem] font-system text-5xl text-wrap w-80 text-center">
+                                Bem-vindo ao <strong className='bg-clip-text text-transparent bg-gradient-to-r from-violet-500 to-fuchsia-500'>Study-io</strong>
+                            </h1>
+                            <p className="font-system text-[1.1rem] w-[36vw] text-justify">👉 O Study-io é um aplicativo de produtividade cuidadosamente projetado para estudantes e trabalhadores de todas as áreas e níveis de ensino.</p>
 
-                            {/* validação de erro dos inputs */}
-                            {errors.username && 
-                                <p className='text-red-500'>
-                                    {errors.username.message}
-                                </p>
-                            }
+                            <p className="font-system text-[1.1rem] w-[36vw] text-justify">👉 Com o Study-io, você pode criar, organizar e priorizar suas tarefas de estudo e trabalho em uma interface simples e intuitiva. Nunca mais perca o foco do que realmente importa.</p>
+
+                            <h1  className="font-roboto text-[2.5rem] font-medium text-wrap text-center">🌌Venha Conosco!🌌</h1>
                         </div>
 
-                        <div className="flex flex-col gap-2 mb-5">
-                            <label className="text-lg">Email:</label>
-                            <input 
-                            type="text" 
-                            placeholder="Digite seu email..." 
-                            className={`text-black rounded-sm w-[25vw] outline-none pl-2 py-2 ${errors.email ? 'border-2 border-red-500' : 'border-none'}`} 
-                            {...register("email")}/>
+                        <form className="w-1/2 flex flex-col justify-center items-center" onSubmit={handleSubmit(singUp)}>
+                            <legend className='mb-7 font-bold font-roboto text-3xl'>Sing Up</legend>
+                            
+                            <div className="flex flex-col gap-2 mb-5">
+                                <label className="text-lg">Username:</label>
+                                <input type="text" 
+                                placeholder="Digite seu nome..." 
+                                className={`text-black rounded-sm w-[25vw] outline-none pl-2 py-2 ${errors.username ? 'border-2 border-red-500' : 'border-none'}`} 
+                                {...register("username")}
+                                />
 
-                            {/* validação de erro dos inputs */}
-                            {errors.email && 
-                                <p className='text-red-500 text-wrap'>
-                                    {errors.email.message}
-                                </p>}
-                        </div>
+                                {/* validação de erro dos inputs */}
+                                {errors.username && 
+                                    <p className='text-red-500'>
+                                        {errors.username.message}
+                                    </p>
+                                }
+                            </div>
 
-                        <div className="flex flex-col gap-2 mb-5">
-                            <label className="text-lg">Password:</label>
-                            <input 
-                            type="password" 
-                            placeholder="Digite sua senha" 
-                            className={`text-black rounded-sm w-[25vw] outline-none pl-2 py-2 ${errors.password ? 'border-2 border-red-500' : 'border-none'}`}  
-                            {...register("password")}
-                            />
+                            <div className="flex flex-col gap-2 mb-5">
+                                <label className="text-lg">Email:</label>
+                                <input 
+                                type="text" 
+                                placeholder="Digite seu email..." 
+                                className={`text-black rounded-sm w-[25vw] outline-none pl-2 py-2 ${errors.email ? 'border-2 border-red-500' : 'border-none'}`} 
+                                {...register("email")}/>
 
-                            {/* validação de erro dos inputs */}
-                            {errors.password && 
-                                <p className='text-red-500 text-wrap'>
-                                    {errors.password.message}
-                                </p>}
-                        </div>
+                                {/* validação de erro dos inputs */}
+                                {errors.email && 
+                                    <p className='text-red-500 text-wrap'>
+                                        {errors.email.message}
+                                    </p>}
+                            </div>
 
-                        <div className="flex flex-col gap-2 mt-2">
-                            <button className="text-white bg-[#5356ad] w-[25vw] py-2 rounded-sm">
-                                Cadastrar
-                            </button>
+                            <div className="flex flex-col gap-2 mb-5">
+                                <label className="text-lg">Password:</label>
+                                <input 
+                                type="password" 
+                                placeholder="Digite sua senha" 
+                                className={`text-black rounded-sm w-[25vw] outline-none pl-2 py-2 ${errors.password ? 'border-2 border-red-500' : 'border-none'}`}  
+                                {...register("password")}
+                                />
 
-                            <p className='text-center'>Ja possui uma conta ? <Link to='/' className='text-violet-600'>Entrar agora</Link></p>
-                        </div>
-                    </form>
-                </div>
+                                {/* validação de erro dos inputs */}
+                                {errors.password && 
+                                    <p className='text-red-500 text-wrap'>
+                                        {errors.password.message}
+                                    </p>}
+                            </div>
+
+                            <div className="flex flex-col gap-2 mt-2">
+                                <button className="text-white bg-[#5356ad] w-[25vw] py-2 rounded-sm">
+                                    Cadastrar
+                                </button>
+
+                                <p className='text-center'>Ja possui uma conta ? <Link to='/' className='text-violet-600'>Entrar agora</Link></p>
+                            </div>
+                        </form>
+                    </div>
+                )}
             </main>
         </RightClick>
     )
