f3055548b17dfaa2d7a97bc0a28ba64ad9b732f8
Thomaz Alves <thomazhilario5@gmail.com>
2024-03-07 15:42:27 -0300

    Alterando component rightClick

--- src/Components/rightClick.tsx
+++ src/Components/rightClick.tsx
@@ -9,26 +9,11 @@ export const RightClick = ({children}:ChildrenType) => {
             </ContextMenu.Trigger>
         <ContextMenu.Content color="indigo">
             
-            <ContextMenu.Item shortcut="⌘ E">Edit</ContextMenu.Item>
-            <ContextMenu.Item shortcut="⌘ D">Duplicate</ContextMenu.Item>
-            <ContextMenu.Separator />
-            <ContextMenu.Item shortcut="⌘ N">Archive</ContextMenu.Item>
-
-            <ContextMenu.Sub>
-            <ContextMenu.SubTrigger>More</ContextMenu.SubTrigger>
-            <ContextMenu.SubContent>
-                <ContextMenu.Item>Move to project…</ContextMenu.Item>
-                <ContextMenu.Item>Move to folder…</ContextMenu.Item>
-                <ContextMenu.Separator />
-                <ContextMenu.Item>Advanced options…</ContextMenu.Item>
-            </ContextMenu.SubContent>
-            </ContextMenu.Sub>
+            <ContextMenu.Item>Refresh</ContextMenu.Item>
 
             <ContextMenu.Separator />
-            <ContextMenu.Item>Share</ContextMenu.Item>
-            <ContextMenu.Item>Add to favorites</ContextMenu.Item>
-            <ContextMenu.Separator />
-            <ContextMenu.Item shortcut="⌘ ⌫" color="red">
+
+            <ContextMenu.Item shortcut="⌘" color="red">
             Delete
             </ContextMenu.Item>
             
