95ec9e22ec361bbf0791dee35ea63d6c7598bc71
Thomaz Alves <thomazhilario5@gmail.com>
2024-03-11 16:40:45 -0300

    Feature para sair da conta ao clicar em log Out

--- src/Components/menu-dialog.tsx
+++ src/Components/menu-dialog.tsx
@@ -1,10 +1,37 @@
 // import Dialog from radix
 import * as Dialog from '@radix-ui/react-dialog'
 
+// react-router-dom
+import { useNavigate } from 'react-router-dom'
+
 // import user from lucide
 import { User } from 'lucide-react'
 
+// import firebase
+import { auth } from '../Services/FirebaseConnection'
+import { signOut } from 'firebase/auth'
+
 export const MenuDialog = () => {
+
+    // navigate
+    const navigate = useNavigate()
+
+    // Saindo da conta
+    async function LogOut(){
+        try {
+            // Retirando autenticacao
+            await signOut(auth)
+
+            // Retirando id da localStorage
+            localStorage.removeItem('@DataId')
+
+            // Navegando a rota inicial
+            navigate('/')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return(
         <Dialog.Root>
 
@@ -16,7 +43,7 @@ export const MenuDialog = () => {
                 <Dialog.Content className='absolute right-2 top-[4.5rem]'>
                     <menu className='py-2 w-[13vw] bg-slate-800 rounded-sm'>
                         <li><button className='p-2 text-center text-white w-full'>My Account</button></li>
-                        <li><button className='p-2 text-center text-white w-full'>Log Out</button></li>
+                        <li><button className='p-2 text-center text-white w-full' onClick={LogOut}>Log Out</button></li>
                     </menu>
                 </Dialog.Content>
             </Dialog.Portal>
