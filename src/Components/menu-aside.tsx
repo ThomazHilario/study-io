229a36778a26a16f063124a73330e194c171b61e
Thomaz Alves <thomazhilario5@gmail.com>
2024-03-14 13:32:32 -0300

    Atrelando onclick ao icon penciline para mudar o valor do booleano da state

--- src/Components/menu-aside.tsx
+++ src/Components/menu-aside.tsx
@@ -1,7 +1,14 @@
 // import Lucide-icons
 import { AlarmClock, PencilLine, NotebookPen, CalendarDays } from 'lucide-react'
 
+// context
+import { UseMyContext } from '../Context/context'
+
 export const MenuAside = () => {
+
+    // Context
+    const {isTask, setIsTask} = UseMyContext()
+
     return(
         <menu className='mt-4 flex flex-col gap-3 *:cursor-pointer'>
             <li className='flex flex-col justify-center items-center hover:bg-gray-400/10 py-[0.15rem] px-1 rounded-md'>
@@ -10,7 +17,7 @@ export const MenuAside = () => {
             </li>
 
             <li className='flex flex-col justify-center items-center hover:bg-gray-400/10 py-[0.15rem] px-1 rounded-md'>
-                <PencilLine color='white' size={20}/>
+                <PencilLine color='white' size={20} onClick={() => setIsTask(!isTask)}/>
                 <p className='text-white text-[0.8rem] font-semibold font-system'>Task</p>
             </li>
 
