0c9baee1011db6cafd4fa161d9f3c14c2ede40ee
Thomaz Alves <thomazhilario5@gmail.com>
2024-03-26 13:33:49 -0300

    feat: menu-asidee
    - Adicionando onclick a div de timer, para alterar o valor do state isTimer.
    - Adicionar renderização com base em state para ver se o timer está redenrizado atraves do background.

--- src/Components/menu-aside.tsx
+++ src/Components/menu-aside.tsx
@@ -7,12 +7,13 @@ import { UseMyContext } from '../Context/context'
 export const MenuAside = () => {
 
     // Context
+    const {isTimer, setIsTimer} = UseMyContext()
     const {isTask, setIsTask} = UseMyContext()
     const {isNotes, setIsNotes} = UseMyContext()
 
     return(
         <menu className='mt-4 flex flex-col gap-3 *:cursor-pointer'>
-            <li className='flex flex-col justify-center items-center hover:bg-gray-400/10 py-[0.15rem] px-1 rounded-md'>
+            <li className={`flex flex-col justify-center items-center ${isTimer && 'bg-gray-400/10'} hover:bg-gray-400/10 py-[0.15rem] px-1 rounded-md`} onClick={() => setIsTimer(!isTimer)}>
                 <AlarmClock color='white' size={20}/>
                 <p className='text-white text-[0.8rem] font-semibold font-system'>Timer</p>
             </li>
