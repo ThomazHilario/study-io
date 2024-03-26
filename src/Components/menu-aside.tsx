bd3a17d8bdc12638d184155182693c62fef9f5db
Thomaz Alves <thomazhilario5@gmail.com>
2024-03-26 13:28:11 -0300

    style: Aside
    - Adicionando background caso os quadro sejam renderizados.

--- src/Components/menu-aside.tsx
+++ src/Components/menu-aside.tsx
@@ -17,12 +17,12 @@ export const MenuAside = () => {
                 <p className='text-white text-[0.8rem] font-semibold font-system'>Timer</p>
             </li>
 
-            <li className='flex flex-col justify-center items-center hover:bg-gray-400/10 py-[0.15rem] px-1 rounded-md' onClick={() => setIsTask(!isTask)}>
+            <li className={`flex flex-col justify-center items-center ${isTask && 'bg-gray-400/10'} hover:bg-gray-400/10 py-[0.15rem] px-1 rounded-md`} onClick={() => setIsTask(!isTask)}>
                 <PencilLine color='white' size={20} />
                 <p className='text-white text-[0.8rem] font-semibold font-system'>Task</p>
             </li>
 
-            <li className='flex flex-col justify-center items-center hover:bg-gray-400/10 py-[0.15rem] px-1 rounded-md' onClick={() => setIsNotes(!isNotes)}>
+            <li className={`flex flex-col justify-center items-center ${isNotes && 'bg-gray-400/10'} hover:bg-gray-400/10 py-[0.15rem] px-1 rounded-md`} onClick={() => setIsNotes(!isNotes)}>
                 <NotebookPen color='white' size={20}/>
                 <p className='text-white text-[0.8rem] font-semibold font-system'>Notes</p>
             </li>
