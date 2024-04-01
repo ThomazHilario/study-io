96aa7186d940c244e2c42479eac75969426a1c9a
Thomaz Alves <thomazhilario5@gmail.com>
2024-04-01 16:57:59 -0300

    feat: MenuDialog component
     - Alterando o top do menu ao deixar a tela em fullscreen.

--- src/Components/menu-dialog.tsx
+++ src/Components/menu-dialog.tsx
@@ -4,6 +4,9 @@ import * as Dialog from '@radix-ui/react-dialog'
 // react-router-dom
 import { useNavigate } from 'react-router-dom'
 
+// import Context
+import { UseMyContext } from '../Context/context'
+
 // import user from lucide
 import { User } from 'lucide-react'
 
@@ -15,6 +18,9 @@ import { signOut } from 'firebase/auth'
 import { AccountDialog } from './account-dialog'
 
 export const MenuDialog = () => {
+
+    // Context
+    const { isFullscreen } = UseMyContext()
 
     // navigate
     const navigate = useNavigate()
@@ -43,7 +49,7 @@ export const MenuDialog = () => {
             </Dialog.Trigger>
 
             <Dialog.Portal>
-                <Dialog.Content className='absolute right-2 top-[4.5rem]'>
+                <Dialog.Content className={`absolute right-2 ${isFullscreen ? 'top-[4.5rem]' : 'top-[3.1rem]'}`}>
                     <menu className='py-2 w-[13vw] bg-slate-800 rounded-sm'>
                         <li><AccountDialog/></li>
                         <li><button className='p-2 text-center text-white w-full' onClick={LogOut}>Log Out</button></li>
