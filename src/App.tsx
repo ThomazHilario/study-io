3ba547593ad519d97199b8e3969e060b1f550725
Thomaz Alves <thomazhilario5@gmail.com>
2024-04-01 16:42:51 -0300

    feat: App Component
    - Ao redimensionar a tela, verificando quais eram os valores de x e y do Task-Frame Component. E alterando para valores mais acessiveis em telass menores.

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,30 @@ appWindow.setMinSize(new PhysicalSize(1450,900))
 // Ao redimensionar a tela
 appWindow.onResized(() => {
 
+  //-------- TimerDrag --------//
+
+  // Buscando os valores x e y na localStorage
   const timerDrag = localStorage.getItem('timerDrag') !== null && JSON.parse(localStorage.getItem('timerDrag') as string)
 
+  // Alterando valores x e y de acordo com a condição
   timerDrag.x = timerDrag.x > 812 ? 800 : timerDrag.x
   timerDrag.y = timerDrag.y > 546 ? 540 : timerDrag.y
 
+  // Salvando na localStorage
   localStorage.setItem('timerDrag',JSON.stringify(timerDrag))
+
+
+  //-------- TaskFrameDrag --------//
+
+  // Buscando valores x e y na localStorage
+  const TaskFrameDrag = localStorage.getItem('TaskFrameDrag') !== null && JSON.parse(localStorage.getItem('TaskFrameDrag') as string)
+  console.log(TaskFrameDrag)
+  // Alterando valores x e y de acordo com a condição
+  TaskFrameDrag.x = TaskFrameDrag.x > 812 ? 730 : TaskFrameDrag.x
+  TaskFrameDrag.y = TaskFrameDrag.y > 546 ? 540 : TaskFrameDrag.y
+
+  // Salvando na localStorage
+  localStorage.setItem('TaskFrameDrag',JSON.stringify(TaskFrameDrag))
 })
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
