6ff7a525a18f4e83b823d8d27b8b544557c2c6d0
Thomaz Alves <thomazhilario5@gmail.com>
2024-04-11 19:08:57 -0300

    feat: Themes Component
    - Criando o desing do container do componente Theme.
    - Deixando a altura e largura adaptaveis a telas fullscreen e telas não fullscreen.

--- src/Components/themes.tsx
+++ src/Components/themes.tsx
@@ -1,7 +1,14 @@
+// import Context
+import { UseMyContext } from "../Context/context"
+
 export const Themes = () => {
+    
+    // Context
+    const { isFullscreen } = UseMyContext()
+
     return(
-        <div>
-            
+        <div className={`absolute h-[88%] w-[20vw] left-[4rem] top-3/4 ${isFullscreen ? '-translate-y-[74%]' : '-translate-y-[75%]'} bg-slate-800 rounded-md`}>
+
         </div>
     )
 }
