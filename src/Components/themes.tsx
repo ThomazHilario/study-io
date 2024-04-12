68e8ac6d099ab7085d9a17912dd773b434a8357f
Thomaz Alves <thomazhilario5@gmail.com>
2024-04-12 18:27:33 -0300

    feat: Themes Component
    - Criando logica de renderização do componente.

--- src/Components/themes.tsx
+++ src/Components/themes.tsx
@@ -1,14 +1,19 @@
 // import Context
 import { UseMyContext } from "../Context/context"
 
+// import lucide-icons
+import { Minus } from 'lucide-react'
+
 export const Themes = () => {
     
     // Context
-    const { isFullscreen } = UseMyContext()
+    const { isFullscreen, setIsThemes } = UseMyContext()
 
     return(
         <div className={`absolute h-[88%] w-[20vw] left-[4rem] top-3/4 ${isFullscreen ? '-translate-y-[74%]' : '-translate-y-[75%]'} bg-slate-800 rounded-md`}>
-
+            <div className="p-6 flex justify-end items-center">
+                <Minus className="cursor-pointer" color="white" onClick={() => setIsThemes(false)}/>
+            </div>
         </div>
     )
 }
