75da02461d80d14e9f959f89620d38e160874877
Thomaz Alves <thomazhilario5@gmail.com>
2024-04-19 18:16:18 -0300

    Style: Gemini Componennt
    - Adicionando border radius as divs.
    - Adicionando span para indicar funcionalidade em beta.

--- src/Components/gemini.tsx
+++ src/Components/gemini.tsx
@@ -37,13 +37,16 @@ export const Gemini = () => {
     }
 
     return(
-        <div className='w-[500px] top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 absolute bg-slate-800 flex flex-col justify-between p-2 text-white'>
+        <div className='w-[500px] top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 absolute bg-slate-800 flex flex-col justify-between p-2 text-white rounded-sm'>
+            <div className='flex justify-end'>
+                <span className='py-1 px-3 bg-black/20 rounded-sm'>Beta</span>
+            </div>
             <div>
-                {response !== '' ? <p>Gemini:{response}</p> : <p>Ola eu sou a Gemini!!</p>}
+                {response !== '' ? <p>Gemini:{response}</p> : <h1 className='text-3xl'>Ola eu sou a Gemini!!</h1>}
             </div>
 
             <div className='flex gap-2'>
-                <textarea className='w-full resize-none bg-black/20 outline-none p-2 overflow-y-hidden' value={question}  rows={1} onChange={(e) => setQuestion(e.target.value)}></textarea>
+                <textarea className='w-full resize-none bg-black/20 outline-none p-2 overflow-y-hidden rounded-sm' value={question}  rows={1} onChange={(e) => setQuestion(e.target.value)}></textarea>
 
                 <button onClick={questionUser}><SendHorizonal/></button>
             </div>
