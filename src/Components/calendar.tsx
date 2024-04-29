d23599e6316cbd5a2020b2e614cbdbad53630ad5
Thomaz Alves <thomazhilario5@gmail.com>
2024-04-29 14:07:30 -0300

    style: Calendar Component
    - Deixando o componente calendar mais agradavel visualmente.

--- src/Components/calendar.tsx
+++ src/Components/calendar.tsx
@@ -1,12 +1,20 @@
-// import datefns
-import { format } from "date-fns";
-
 // import DayPicker
+import { ptBR } from 'date-fns/locale'
 import { DayPicker } from 'react-day-picker'
-
+import 'react-day-picker/dist/style.css';
 export const Calendar = () => {
 
     return(
-        <DayPicker className="absolute top-10" mode="single"/>
+            <DayPicker 
+            className='absolute bg-slate-800 top-60 rounded-md py-2 px-5 text-white' 
+            mode="single"
+            locale={ptBR}
+            modifiersStyles={{
+                today: {
+                    backgroundColor: 'rgba(59, 130, 246, 1)',
+                    color: 'white',
+                },
+            }}
+            />
     )
 }
