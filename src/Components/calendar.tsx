cc7d61d7e6c0319adc741fefe7739d5e0bdf8ca3
Thomaz Alves <thomazhilario5@gmail.com>
2024-05-02 15:19:38 -0300

    feat: Calendar Component
    - Criando uma function que desconstroi componente ao clicar no icom Minus.

--- src/Components/calendar.tsx
+++ src/Components/calendar.tsx
@@ -7,15 +7,20 @@ import { ptBR } from 'date-fns/locale'
 // import css from datePicker Calendar
 import 'react-day-picker/dist/style.css';
 
+// import Context
+import { UseMyContext } from '../Context/context';
+
 // import lucide-icons
 import { Minus } from 'lucide-react'
 
 export const Calendar = () => {
+    // Context 
+    const { setIsCalendar } = UseMyContext()
 
     return(
         <div className='absolute bg-slate-800/95 top-80 left-16 rounded-md text-white'>
             <div className='border-b-[1px] flex justify-end px-3 py-1'>
-                <Minus className='cursor-pointer' color='white'/>
+                <Minus className='cursor-pointer' color='white' onClick={() => setIsCalendar(false)}/>
             </div>
 
             <DayPicker 
