769cde384d86929587b5f8182947cfec92435566
Thomaz Alves <thomazhilario5@gmail.com>
2024-05-07 18:08:31 -0300

    feat:RightClick Componente
    - Adicionando mais dois ContextMenu.Item para a renderização dos componentes Calendar e Themes.

--- src/Components/rightClick.tsx
+++ src/Components/rightClick.tsx
@@ -12,9 +12,13 @@ export const RightClick = ({children}:ChildrenType) => {
         setIsTask, 
         setIsNotes, 
         setIsTimer,
+        setIsThemes,
+        setIsCalendar,
         isTask,
         isNotes,
-        isTimer 
+        isTimer,
+        isThemes,
+        isCalendar 
     } = UseMyContext()
 
     return(
@@ -26,17 +30,25 @@ export const RightClick = ({children}:ChildrenType) => {
 
             {isLogged ? (
                 <>
-                <ContextMenu.Item shortcut="⌘" color="violet" onClick={() => setIsTask(!isTask)}>
-                    {isTask ? 'Close Task' : 'Add Task'}
-                </ContextMenu.Item>
+                    <ContextMenu.Item shortcut="⌘" color="violet" onClick={() => setIsTask(!isTask)}>
+                        {isTask ? 'Close Task' : 'Add Task'}
+                    </ContextMenu.Item>
 
-                <ContextMenu.Item shortcut="⌘" color="violet" onClick={() => setIsNotes(!isNotes)}>
-                    {isNotes ? 'Close Notes' : 'Add Notes'}
-                </ContextMenu.Item>
+                    <ContextMenu.Item shortcut="⌘" color="violet" onClick={() => setIsNotes(!isNotes)}>
+                        {isNotes ? 'Close Notes' : 'Add Notes'}
+                    </ContextMenu.Item>
 
-                <ContextMenu.Item shortcut='⌘' color="violet" onClick={() => setIsTimer(!isTimer)}>
-                    {isTimer ? 'Close Timer' : 'Open Timer'}
-                </ContextMenu.Item>
+                    <ContextMenu.Item shortcut='⌘' color="violet" onClick={() => setIsTimer(!isTimer)}>
+                        {isTimer ? 'Close Timer' : 'Open Timer'}
+                    </ContextMenu.Item>
+
+                    <ContextMenu.Item shortcut='⌘' color="violet" onClick={() => setIsThemes(!isThemes)}>
+                        {isThemes ? 'Close Themes' : 'Open Themes'}
+                    </ContextMenu.Item>
+
+                    <ContextMenu.Item shortcut='⌘' color="violet" onClick={() => setIsCalendar(!isCalendar)}>
+                        {isCalendar ? 'Close Calendar' : 'Open Calendar'}
+                    </ContextMenu.Item>
                 </>
             ) : (
                 <ContextMenu.Item>Refresh</ContextMenu.Item>
