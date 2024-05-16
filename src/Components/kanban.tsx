0a0698469579af75dd8cb9a305e10b30df21c86d
Thomaz Alves <thomazhilario5@gmail.com>
2024-05-16 16:01:12 -0300

    Adicionando outra biblioteca

--- src/Components/kanban.tsx
+++ src/Components/kanban.tsx
@@ -1,24 +1,39 @@
+import { DragDropContext, Draggable, Droppable } from '@hello-pangea/dnd'
+import { useState } from 'react'
+
 export const Kanban = () => {
+
+    const [taskKanban, setTaskKanban] = useState([{
+        tasl:'fazer tal coisa'
+    }])
+
+    function taskDragEnd(result:any){
+        console.log(result)
+    }
+
     return(
-        <div className="absolute flex gap-2 justify-around top-[50%] left-[52%] -translate-x-1/2 -translate-y-[45%]  w-[90vw] h-[80vh] bg-black/30 rounded-sm p-1">
-            <div className="w-[284px] bg-slate-900/95 rounded-md h-min p-2">
-                <p className=" text-white rounded-sm font-system ">Tarefas a fazer:</p>
-                <button className="hover:bg-white/5 text-white w-full text-start mt-2 rounded-md p-1">+ Adicionar Tarefa</button>
-            </div>
+        
+            <div className="absolute flex gap-2 justify-around top-[50%] left-[52%] -translate-x-1/2 -translate-y-[45%]  w-[90vw] h-[80vh] bg-black/30 rounded-sm p-1">
+                <div className="w-[284px] bg-slate-900/95 rounded-md h-min p-2">
+                    <p className=" text-white rounded-sm font-system ">Tarefas a fazer:</p>
+                    
+                    <button className="hover:bg-white/5 text-white w-full text-start mt-2 rounded-md p-1">+ Adicionar Tarefa</button>
+                </div>
 
-            <div className="w-[284px] bg-slate-900/95 rounded-md h-min p-2">
-                <p className=" text-white rounded-sm font-system">Em Desenvolvimento:</p>
-                <button className="hover:bg-white/5 text-white w-full text-start mt-2 rounded-md p-1">+ Adicionar Tarefa</button>
-            </div>
+                <div className="w-[284px] bg-slate-900/95 rounded-md h-min p-2">
+                    <p className=" text-white rounded-sm font-system">Em Desenvolvimento:</p>
+                    <button className="hover:bg-white/5 text-white w-full text-start mt-2 rounded-md p-1">+ Adicionar Tarefa</button>
+                </div>
 
-            <div className="w-[284px] bg-slate-900/95 rounded-md h-min p-2">
-                <p className=" text-white rounded-sm font-system">Pausado:</p>
+                <div className="w-[284px] bg-slate-900/95 rounded-md h-min p-2">
+                    <p className=" text-white rounded-sm font-system">Pausado:</p>
+                    <button className="hover:bg-white/5 text-white w-full text-start mt-2 rounded-md p-1">+ Adicionar Tarefa</button>
+                </div>
+                <div className="w-[284px] bg-slate-900/95 rounded-md h-min p-2">
+                <p className=" text-white rounded-sm font-system">Concluidos:</p>
                 <button className="hover:bg-white/5 text-white w-full text-start mt-2 rounded-md p-1">+ Adicionar Tarefa</button>
+                </div>
             </div>
-            <div className="w-[284px] bg-slate-900/95 rounded-md h-min p-2">
-            <p className=" text-white rounded-sm font-system">Concluidos:</p>
-            <button className="hover:bg-white/5 text-white w-full text-start mt-2 rounded-md p-1">+ Adicionar Tarefa</button>
-            </div>
-        </div>
+          
     )
 }
