fda30483842d67dd6de0ce676ec03479203bf40c
Thomaz Alves <thomazhilario5@gmail.com>
2024-07-17 17:37:49 -0300

    style: ColumnKanban.tsx
    - Adicionando variável com notações de tailwind nas sections.

--- src/Components/ColumnKanban.tsx
+++ src/Components/ColumnKanban.tsx
@@ -8,12 +8,18 @@ import { CardKanban } from "./CardKanban"
 import { ColumnsKanbanProps } from "../interfaces/kanbanTypes"
 
 export const ColumnsKanban = ({task, devTask, pauseTask, completeTask}:ColumnsKanbanProps) => {
+    // sectionColumnStyle
+    const sectionColumnStyle = 'h-min w-[290px] bg-slate-900/90 rounded-sm px-5 py-2 text-white'
     return(
         <div className="flex gap-2 justify-around">
             <Droppable droppableId="fazer" direction="vertical">
                 {(provider) => {
                     return(
-                        <section ref={provider.innerRef} {...provider.droppableProps} className='h-min w-[290px] bg-slate-900/90 rounded-sm px-5 py-2 text-white'>
+                        <section 
+                            ref={provider.innerRef} 
+                            {...provider.droppableProps} 
+                            className={sectionColumnStyle}>
+
                             <h1 className='mb-2'>Tarefas a fazer:</h1>
 
                             {task.map((task,index) => (
@@ -33,7 +39,11 @@ export const ColumnsKanban = ({task, devTask, pauseTask, completeTask}:ColumnsKa
             <Droppable droppableId="desenvolvendo" direction="vertical">
                 {(provider) => {
                     return(
-                        <section ref={provider.innerRef} {...provider.droppableProps} className='h-min w-[290px] bg-slate-900/90 rounded-sm px-5 py-2 text-white'>
+                        <section 
+                            ref={provider.innerRef} 
+                            {...provider.droppableProps} 
+                            className={sectionColumnStyle}>
+
                             <h1 className='mb-2'>Em desenvolvendo:</h1>
 
                             {devTask.length > 0 && devTask.map((task, index) => (
@@ -56,7 +66,8 @@ export const ColumnsKanban = ({task, devTask, pauseTask, completeTask}:ColumnsKa
                         <section 
                         ref={provider.innerRef} 
                         {...provider.droppableProps} 
-                        className='h-min w-[290px] bg-slate-900/90 rounded-sm px-5 py-2 text-white'>
+                        className={sectionColumnStyle}>
+                            
                             <h1 className='mb-2'>Pausado:</h1>
 
                             {pauseTask.length > 0 && pauseTask.map((task, index) => (
@@ -76,7 +87,11 @@ export const ColumnsKanban = ({task, devTask, pauseTask, completeTask}:ColumnsKa
             <Droppable droppableId="concluido" direction="vertical">
                 {(provider) => {
                     return(
-                        <section ref={provider.innerRef} {...provider.droppableProps} className='h-min w-[290px] bg-slate-900/90 rounded-sm px-5 py-2 text-white'>
+                        <section 
+                            ref={provider.innerRef} 
+                            {...provider.droppableProps} 
+                            className={sectionColumnStyle}>
+
                             <h1 className='mb-2'>Concluido:</h1>
 
                             {completeTask.length > 0 && completeTask.map((task,index) => (
