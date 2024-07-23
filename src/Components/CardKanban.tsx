d3c7a506e34af34c4e7d5bf4bbdde731b7bc1440
Thomaz Alves <thomazhilario5@gmail.com>
2024-07-23 12:33:35 -0300

    Style:Card.kanban.tsx
    - Criando o estilo daa paarte de edição da task;

--- src/Components/CardKanban.tsx
+++ src/Components/CardKanban.tsx
@@ -1,15 +1,25 @@
+// React
+import { useState } from "react"
+
 // React-dnd
 import { Draggable } from "@hello-pangea/dnd"
 
 // radix-ui
 import * as DialogAlert from '@radix-ui/react-alert-dialog'
 
+// lucide-React
+import { X } from "lucide-react"
+
 // Interface
 import { CardKanbanProps } from "../interfaces/kanbanTypes"
 
 export const CardKanban = ({index, task, tasks, setTask}:CardKanbanProps) => {
 
-    function deleteTask(){
+    // state - isEdit
+    const [isEditTask, setIsEditTask] = useState<boolean>(false)
+
+    // handleDeleteTask
+    function handleDeleteTask(){
         setTask(tasks.filter(taskArray => taskArray.id !== task.id))
     }
 
@@ -37,18 +47,30 @@ export const CardKanban = ({index, task, tasks, setTask}:CardKanbanProps) => {
                                 <DialogAlert.Overlay className="fixed inset-0 bg-black/50"/>
 
                                 {/* Content Dialog */}
-                                <DialogAlert.Content className="absolute top-1/2 left-[42%] text-white">
-                                    <section className="bg-gray-700 px-5 py-2 rounded-sm">
-                                        <p className="bg-black/30 p-2 px-5 rounded-sm">{task.name}</p>
+                                <DialogAlert.Content className="absolute top-1/2 left-[37%] text-white">
+                                    <section className="bg-gray-700 px-5 pb-2 rounded-sm w-[30vw]">
+                                        <DialogAlert.Cancel className="absolute bg-gray-800 rounded-sm right-0 p-1">
+                                            <X color="white" size={20}/>
+                                        </DialogAlert.Cancel>
+
+                                        <article className="pt-10">
+                                            {isEditTask ? (
+                                                <form className="w-full">
+                                                    <textarea className="p-2 w-full resize-none bg-black/40"/>
+                                                </form>
+                                            ):(
+                                                <p className="bg-black/30 p-2 px-5 rounded-sm">
+                                                    {task.name}
+                                                </p>
+                                            )}
+                                        </article>
 
                                         {/* Buttons */}
                                         <div className="flex justify-between mt-5">
-                                            <DialogAlert.Cancel className="bg-gray-800 px-5 py-1 rounded-sm">
-                                                Sair
-                                            </DialogAlert.Cancel>
+                                            <button className="bg-green-500 h-8 w-20 rounded-sm" onClick={() => setIsEditTask(!isEditTask)}>Editar</button>
 
-                                            <DialogAlert.Cancel className='bg-red-500 px-5 py-1 rounded-sm' onClick={deleteTask}>
-                                                Delete
+                                            <DialogAlert.Cancel className='bg-red-500 h-8 w-20  rounded-sm' onClick={!isEditTask ? handleDeleteTask : () => setIsEditTask(false)}>
+                                                {isEditTask ? 'Cancelar' : 'Delete'}
                                             </DialogAlert.Cancel>
                                         </div>
 
