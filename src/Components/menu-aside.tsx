2c76724327133976ef3ce0323436c4a5a7f5cc65
Thomaz Alves <thomazhilario5@gmail.com>
2024-07-29 10:53:09 -0300

    styled: Menu-Aside.tsx
    - Trocar os valores do tailwind nos elementos de li, por variáveis de estilos do tailwind.

--- src/Components/menu-aside.tsx
+++ src/Components/menu-aside.tsx
@@ -14,34 +14,37 @@ export const MenuAside = () => {
     const { isCalendar, setIsCalendar } = UseMyContext()
     const { isKanban, setIsKanban } = UseMyContext()
 
+    // Styled list
+    const liStyledWithTailwind = 'flex flex-col justify-center items-center hover:bg-gray-400/10 py-[0.15rem] px-1 rounded-md'
+
     return(
         <menu className='mt-4 flex flex-col gap-3 *:cursor-pointer'>
-            <li className={`flex flex-col justify-center items-center ${isTimer && 'bg-gray-400/10'} hover:bg-gray-400/10 py-[0.15rem] px-1 rounded-md`} onClick={() => setIsTimer(!isTimer)}>
+            <li className={`${liStyledWithTailwind} ${isTimer && 'bg-gray-400/10'}`} onClick={() => setIsTimer(!isTimer)}>
                 <AlarmClock color='white' size={20}/>
                 <p className='text-white text-[0.8rem] font-semibold font-system'>Timer</p>
             </li>
 
-            <li className={`flex flex-col justify-center items-center ${isTask && 'bg-gray-400/10'} hover:bg-gray-400/10 py-[0.15rem] px-1 rounded-md`} onClick={() => setIsTask(!isTask)}>
+            <li className={`${liStyledWithTailwind} ${isTask && 'bg-gray-400/10'}`} onClick={() => setIsTask(!isTask)}>
                 <PencilLine color='white' size={20} />
                 <p className='text-white text-[0.8rem] font-semibold font-system'>Task</p>
             </li>
 
-            <li className={`flex flex-col justify-center items-center ${isNotes && 'bg-gray-400/10'} hover:bg-gray-400/10 py-[0.15rem] px-1 rounded-md`} onClick={() => setIsNotes(!isNotes)}>
+            <li className={`${liStyledWithTailwind} ${isNotes && 'bg-gray-400/10'}`} onClick={() => setIsNotes(!isNotes)}>
                 <NotebookPen color='white' size={20}/>
                 <p className='text-white text-[0.8rem] font-semibold font-system'>Notes</p>
             </li>
 
-            <li className={`flex flex-col justify-center items-center ${isThemes && 'bg-gray-400/10'} hover:bg-gray-400/10 py-[0.15rem] px-1 rounded-md`} onClick={() => setIsThemes(!isThemes)}>
+            <li className={`${liStyledWithTailwind} ${isThemes && 'bg-gray-400/10'}`} onClick={() => setIsThemes(!isThemes)}>
                 <WallpaperIcon color='white' size={20}/>
                 <p className='text-white text-[0.75rem] font-semibold font-system'>Themes</p>
             </li>
 
-            <li className={`flex flex-col justify-center items-center hover:bg-gray-400/10 py-[0.15rem] px-1 rounded-md ${isCalendar && 'bg-gray-400/10'}`} onClick={() => setIsCalendar(!isCalendar)}>
+            <li className={`${liStyledWithTailwind} ${isCalendar && 'bg-gray-400/10'}`} onClick={() => setIsCalendar(!isCalendar)}>
                 <CalendarDays color='white' size={20}/>
                 <p className='text-white text-[0.8rem] font-semibold font-system'>Cal</p>
             </li>
 
-            <li className={`flex flex-col justify-center items-center hover:bg-gray-200/10 py-[0.15rem] px-1 rounded-md ${isKanban && 'bg-gray-400/10'}`} onClick={() => setIsKanban(!isKanban)}>
+            <li className={`${liStyledWithTailwind} ${isKanban && 'bg-gray-400/10'}`} onClick={() => setIsKanban(!isKanban)}>
                 <Kanban color='white' size={20}/>
                 <p className='text-white text-[0.8rem] font-semibold font-system'>Kanban</p>
             </li>
