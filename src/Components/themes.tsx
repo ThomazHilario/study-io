d40ccea02c364f1c51fff1ae1aef41d35d757d95
Thomaz Alves <thomazhilario5@gmail.com>
2024-11-05 10:49:42 -0300

    feat(Themes.tsx): Create loading
    - Create loading charge in theme component.

--- src/Components/themes.tsx
+++ src/Components/themes.tsx
@@ -58,7 +58,12 @@ export const Themes = () => {
 
             {/* Loading */}
             {isPending && (
-                <h2 className="text-center text-white">Loading themes</h2>
+                <section className="flex justify-center items-center w-full h-2/4 font-roboto">
+                    <button type="button" className="flex justify-center items-center px-7 py-3" disabled>
+                        <svg className="border-[9px] rounded-full border-dotted border-white animate-spin size-9 mr-3 " viewBox="0 0 24 24"/>
+                        <h1 className='text-3xl text-white'>Loading...</h1>
+                    </button>
+                </section>
             )}
         </section>
     )
