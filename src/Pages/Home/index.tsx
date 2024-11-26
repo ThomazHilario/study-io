72bcba9ba61b3fce039349b816a864be6b798ba1
Thomaz Alves <thomazhilario5@gmail.com>
2024-11-26 11:30:57 -0300

    Style: Tailwind code organizing

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -105,25 +105,45 @@ export const Home = () => {
         }
     }
 
-    // Container forms style for Tailwind
-    const styleContainerInputsForm = 'flex flex-col gap-2 mb-5'
+    // ---------------------- Tailwind Style ----------------------------- //
+        // Section Container
+        const sectionContainer = 'text-white flex justify-center items-center h-[90vh] w-[90vw] rounded-2xl bg-[#202124]'
 
-    // Inputs style for Tailwind
-    const styleInputsForms = 'text-white rounded-sm w-[25vw] outline-none pl-2 py-2  bg-transparent bg-gradient-to-r from-gray-700/10 to-gray-300/5 border-2 placeholder:text-white'
+        // Description from article
+        const descriptionParagraph = 'font-system text-[1.1rem] w-[36vw] text-justify'
 
+        // Container forms style for Tailwind
+        const styleContainerInputsForm = 'flex flex-col gap-2 mb-5'
+
+        // Inputs style for Tailwind
+        const styleInputsForms = (error:unknown) => {
+
+            // Case have error
+            if(error){
+                return 'text-white rounded-sm w-[25vw] outline-none pl-2 py-2  bg-transparent bg-gradient-to-r from-gray-700/10 to-gray-300/5 border-2 placeholder:text-white border-red-500'
+            }
+
+            // Case not have error
+            return 'text-white rounded-sm w-[25vw] outline-none pl-2 py-2  bg-transparent bg-gradient-to-r from-gray-700/10 to-gray-300/5 border-2 placeholder:text-white border-zinc-700/40'
+        }
+    // -------------------------------------------------------------------- //
     return (
         <RightClick>
             <main className=" flex justify-center items-center h-screen bg-[#5356ad]">
                 {/* container do formulario de login */}
                 {loading ? <Loading/> : (
-                    <section className=" text-white flex justify-center items-center h-[90vh] w-[90vw] rounded-2xl bg-[#202124]">
+                    <section className={sectionContainer}>
                         <article className="w-1/2 flex flex-col justify-center items-center gap-10 ">
+                            {/* Title */}
                             <h1 className="leading-[3.5rem] font-system text-5xl w-80 text-center">
                                 Bem-vindo ao <strong className='bg-clip-text text-transparent bg-gradient-to-r from-violet-500 to-fuchsia-500'>Study-io</strong>
                             </h1>
-                            <p className="font-system text-[1.1rem] w-[36vw] text-justify">👉 O Study-io é um aplicativo de produtividade cuidadosamente projetado para estudantes e trabalhadores de todas as áreas e níveis de ensino.</p>
 
-                            <p className="font-system text-[1.1rem] w-[36vw] text-justify">👉 Com o Study-io, você pode criar, organizar e priorizar suas tarefas de estudo e trabalho em uma interface simples e intuitiva. Nunca mais perca o foco do que realmente importa.</p>
+                            {/* Description */}
+
+                            <p className={descriptionParagraph}>👉 O Study-io é um aplicativo de produtividade cuidadosamente projetado para estudantes e trabalhadores de todas as áreas e níveis de ensino.</p>
+
+                            <p className={descriptionParagraph}>👉 Com o Study-io, você pode criar, organizar e priorizar suas tarefas de estudo e trabalho em uma interface simples e intuitiva. Nunca mais perca o foco do que realmente importa.</p>
 
                             <h1  className="font-roboto text-[2.5rem] font-medium text-wrap text-center">🌌Venha Conosco!🌌</h1>
                         </article>
@@ -136,7 +156,7 @@ export const Home = () => {
                                 <label className="text-lg">Email:</label>
                                 <input type="text" 
                                 placeholder="Digite seu email..." 
-                                className={`${styleInputsForms} ${errors.email ? 'border-red-500' : 'border-zinc-700/40'}`}
+                                className={styleInputsForms(errors.email)}
                                 {...register('email')}/>
                                 
                                 {/* validação de erros dos inputs */}
@@ -150,7 +170,7 @@ export const Home = () => {
                                 <label className="text-lg">Password:</label>
                                 <input type="password" 
                                 placeholder="Digite sua senha" 
-                                className={`${styleInputsForms} ${errors.password ? 'border-red-500' : 'border-zinc-700/40'}`}
+                                className={styleInputsForms(errors.password)}
                                 {...register('password')}/>
                                 
                                 {/* validação de erros dos inputs */}
